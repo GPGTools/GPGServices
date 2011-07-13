f499c8b92cc83b586f2170e9b3f7faf2158a43e0
Moritz Ulrich <ulrich.moritz@googlemail.com>
2011-07-13 15:17:12 +0200

    Add cell for displaying color in a nice circle (we'll use this for showing the result of a verification)

---
+++ IndicatorCell.h
@@ -0,0 +1,15 @@
+//
+//  IndicatorCell.h
+//  GPGServices
+//
+//  Created by Moritz Ulrich on 13.07.11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <AppKit/AppKit.h>
+
+@interface IndicatorCell : NSActionCell {
+    NSColor* _color; //Access through self.objectValue
+}
+
+@end
