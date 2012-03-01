bc3f8c3d0087d01a9e64d062c305c9bac91a1907
C Fraire <cfraire@me.com>
2012-02-29 20:56:08 -0500

    Improve initialization of KeyChooserDataSource keys and chosen
    - improve handling of an unset default-key; do not masquerade a key
      as default-key
    - allow the Secret Key popup button to initialize as unselected
    - init uses existing KeyChooserDataSource update method from init
      to set available keys and to handle default-key
    - GPGServices:myPrivateKey more simply affirms that its return
      value is secret
    - fix up key description to not include an empty comment
    - allow for spaces in default-key, in case it contains prettified
      output from gpg --fingerprint
    
    - inspired by GPGPreferences, add a build stage to force-soft-link
      target output into ~/Library/Services, for easier testing
    - remove hard-coded FRAMEWORK_SEARCH_PATHS; link already properly
      referenced frameworks

--- Source/KeyChooserDataSource.m
+++ Source/KeyChooserDataSource.m
@@ -11,22 +11,40 @@
 
 @implementation KeyChooserDataSource
 
-@synthesize availableKeys, selectedIndex, keyDescriptions, keyValidator;
+@synthesize availableKeys, keyDescriptions, keyValidator;
 
 - (GPGKey*)selectedKey {
-    if(self.selectedIndex < self.availableKeys.count)
-        return [self.availableKeys objectAtIndex:self.selectedIndex];
-    else
-        return nil;
+    return selectedKey;
 }
 
 - (void)setSelectedKey:(GPGKey *)selKey {
+    if ([selKey isEqual:selectedKey])
+        return;
+
     [self willChangeValueForKey:@"selectedKey"];
+    NSUInteger keyindex = [self.availableKeys indexOfObject:selKey];
     [selectedKey release];
     selectedKey = [selKey retain];
     [self didChangeValueForKey:@"selectedKey"];
     
-    self.selectedIndex = [self.availableKeys indexOfObject:selKey];
+    self.selectedIndex = keyindex;
+}
+
+- (NSInteger)selectedIndex {
+    return selectedIndex_;
+}
+
+- (void)setSelectedIndex:(NSInteger)selectedIndex {
+    if (selectedIndex == selectedIndex_)
+        return;
+
+    [self willChangeValueForKey:@"selectedIndex"];
+    GPGKey *keyobject = (selectedIndex >= 0 && selectedIndex < [self.availableKeys count])
+    ? [self.availableKeys objectAtIndex:selectedIndex] : nil;
+    selectedIndex_ = selectedIndex;
+    [self didChangeValueForKey:@"selectedIndex"];
+
+    self.selectedKey = keyobject;
 }
 
 - (id)init {
@@ -40,14 +58,10 @@
            forKeyPath:@"keyValidator"
               options:NSKeyValueObservingOptionNew
               context:nil];
-    
-    // self.availableKeys = [self getPrivateKeys];
-    self.selectedKey = [self getDefaultKey];
 
-    if(self.selectedIndex == NSNotFound)
-        self.selectedIndex = 0;
-    
-    [self updateDescriptions];
+    selectedIndex_ = -1;
+    self->selectedKey = nil;
+    [self update];
     
     return self;
 }
@@ -55,7 +69,7 @@
 - (void)dealloc {
     [self removeObserver:self forKeyPath:@"availableKeys"];
     [self removeObserver:self forKeyPath:@"keyValidator"];
-    
+
     self.availableKeys = nil;
     self.selectedKey = nil;
     self.keyValidator = nil;
@@ -78,10 +92,9 @@
     NSMutableArray* arr = [NSMutableArray arrayWithCapacity:self.availableKeys.count];
     for(GPGKey* k in self.availableKeys) {
         NSString* c = [k comment];
-        c = c ? [NSString stringWithFormat:@"(%@) ", c] : @"";
+        c = (c && [c length]) ? [NSString stringWithFormat:@"(%@) ", c] : @"";
         [arr addObject:[NSString stringWithFormat:@"%@ - %@ %@<%@>",
                         [k shortKeyID], [k name], c, [k email]]];
-
     }
     
     self.keyDescriptions = arr;
@@ -97,29 +110,26 @@
             return NO;
         }]];
     else {
-        NSLog(@"getPrivateKeys called with keyValidator=%@ using all private keys",self.keyValidator);        
         return keys;
     }
 }
 
 - (GPGKey*)getDefaultKey {
-    GPGKey* key = [GPGServices myPrivateKey];
-    
-    if(key != nil)
-        return key;
-    else if(self.availableKeys.count > 0)
-        return [self.availableKeys objectAtIndex:0];
-    else
-        return nil;
+    return [GPGServices myPrivateKey];
 }
 
 - (void)update {
-    self.availableKeys = [self getPrivateKeys];
-    self.selectedKey = [self getDefaultKey];
-    
-    if(self.selectedIndex == NSNotFound)
-        self.selectedIndex = 0;
-    
+    NSArray *nowAvailableKeys = [self getPrivateKeys];
+    if (self.selectedKey && ![nowAvailableKeys containsObject:self.selectedKey])
+        self.selectedKey = nil;
+    self.availableKeys = nowAvailableKeys;
+
+    GPGKey *nowDefaultKey = [self getDefaultKey];
+    if (!nowDefaultKey)
+        self.selectedKey = nil;
+    else if ([nowAvailableKeys containsObject:nowDefaultKey])
+        self.selectedKey = nowDefaultKey;
+
     [self updateDescriptions];
 }
 
