cdb94b89c504eca7f1aabf4995c9d1b5f45e9174
Mento <Mento@gpgtools.org>
2012-08-12 23:53:59 +0200

    Ivars added.

--- Source/SimpleTextWindow.h
+++ Source/SimpleTextWindow.h
@@ -6,7 +6,10 @@
 - (void)simpleTextWindowWillClose:(SimpleTextWindow *)simpleTextWindow;
 @end
 
-@interface SimpleTextWindow : NSWindowController <NSWindowDelegate>
+@interface SimpleTextWindow : NSWindowController <NSWindowDelegate> {
+	NSObject <SimpleTextWindowDelegate> *delegate;
+	NSString *text, *title;
+}
 @property (assign) NSObject <SimpleTextWindowDelegate> *delegate;
 @property (retain, readonly) NSString *text, *title;
 + (void)showText:(NSString *)text withTitle:(NSString *)title andDelegate:(NSObject <SimpleTextWindowDelegate> *)delegate;
